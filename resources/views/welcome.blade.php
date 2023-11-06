c05982c39f1fb262b03da13239fe116530248a30
Nicolas <vergaralucianonicolas@gmail.com>
2023-11-06 19:50:16 -0300

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -101,12 +101,12 @@
                         
                         </a>
                     @endif-->
-                        <a href="/geshorario/public/login" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <a href="{{ route('login') }}" class="bg-isfd"> <!-- scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"> -->
                             <div> 
-                            <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                                <i class="material-icons">person</i>
+                            <div class="bg-person">
+                                <i class="bi bi-person"></i>
                             </div>
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Login</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">{{ __('Login') }}</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                     Si ya se posee su usuario en la gestion de horario de IFD ingresa aqui.
@@ -119,13 +119,13 @@
                         </a>
             
 
-                        <a href="/geshorario/public/register" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <a href="{{ route('register') }}" class="bg-isfd"> <!-- scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"> -->
                             <div>
-                                <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                                <span class="material-symbols-outlined">app_registration</span>
+                                <div class="bg-person">
+                                    <i class="bi bi-person-add"></i>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Register</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">{{ __('Register') }}</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                     Si desea registrarse o registrar un usuario ingrese aqui
